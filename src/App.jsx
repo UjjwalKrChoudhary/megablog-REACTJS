99c395fb133d8fad52b3172f3496db3209ed93a8
UjjwalKrChoudhary <117009413+UjjwalKrChoudhary@users.noreply.github.com>
2024-05-06 12:41:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+  console.log(process.env.REACT_APP_APPWRITE_URL);
+
+
+  return (
+    <>
+    <h1> mega Blog</h1>
+    </>
+  )
+}
+
+export default App
