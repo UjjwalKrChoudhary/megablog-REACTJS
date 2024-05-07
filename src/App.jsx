623ebf61fb63f519558c0ebd09c120d917737648
UjjwalKrChoudhary <117009413+UjjwalKrChoudhary@users.noreply.github.com>
2024-05-08 01:25:50 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
-import { useState } from 'react'
-
+import { useState,useEffect } from 'react'
+import {useDispatch} from 'react-redux'
+import './App.css'
+import authservice from './appwrite/auth'
+import {login,logout} from "./store/authslice"
+import { Outlet } from 'react-router-dom'
+import { Footer, Header } from './components'
+// import {Footer,Header} from 'react-router-dom'
 
 function App() {
-  console.log(process.env.REACT_APP_APPWRITE_URL);
+  //console.log(import.meta.env.REACT_APP_APPWRITE_URL);
+   const [loading,setLoading]=useState(true)
+  const dispatch=useDispatch()
 
+  useEffect(()=>{
+    authservice.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+       dispatch(login({userData}))  
+      }else{
+        dispatch(logout())
+      }
 
+    })
+    .finally(()=> setLoading(false))
+    
+  },[])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-grey-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          {/* <Outlet /> */}
+        </main>
+        <Footer  />
+      </div>
+    </div>
+  ) : null
   return (
     <>
     <h1> mega Blog</h1>
