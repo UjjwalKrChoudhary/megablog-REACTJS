77f7b62e8e6a1e76c5ce9573b05161a0ed5c9b24
UjjwalKrChoudhary <117009413+UjjwalKrChoudhary@users.noreply.github.com>
2024-05-10 01:26:19 +0530

    commi

---
+++ src/components/Signup.jsx
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'
+import authService from '../appwrite/auth'
+import { Link,useNavigate } from 'react-router-dom'
+import { login } from '../store/authslice'
+import {Button, Input, Logo} from './index'
+import { UseDispatch } from 'react-redux'
+import {useForm} from 'react-hook-form'
+
+function Signup() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Signup
